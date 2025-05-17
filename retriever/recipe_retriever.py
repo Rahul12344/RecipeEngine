705c43cb9b155e0e9535a366cdd633a990826ba2
Rahul12344 <therollingreus@gmail.com>
2025-05-16 23:44:59 -0700

    Add basic pipeline for retrieving recipes from All Recipes

--- retriever/recipe_retriever.py
+++ retriever/recipe_retriever.py
@@ -1,12 +1,34 @@
-# web scraper for recipes. Can batch or gwt single recipe
+from enum import Enum
+from annotater.recipe_annotation_pipeline import RecipeAnnotationPipeline
+
+class Source(Enum):
+    SOURCE_ALL_RECIPES = "all_recipes"
 
 class RecipeRetriever:
-    def __init__(self):
-        pass
+    def __init__(self, recipe_annotation_pipeline: RecipeAnnotationPipeline, recipe_source_retrievers: dict[Source, RecipeSourceRetriever]):
+        self._recipe_annotation_pipeline = recipe_annotation_pipeline
+        self._recipe_source_retrievers = recipe_source_retrievers
 
-    def get_recipe(self, recipe_id: str) -> dict:
-        pass
+    def save_recipe_from_url(self, recipe_url: str, source: Source) -> None:
+        if not (
+            recipe_source_retriever := self._recipe_source_retrievers.get(source)
+        ):
+            raise ValueError(f"No recipe source retriever found for source: {source}")
+        recipes = recipe_source_retriever.get_recipe_from_url(recipe_url)
+        for recipe in recipes:
+            self._recipe_annotation_pipeline(self._get_recipe_text_from_recipe_dict(recipe))
 
-    def get_recipes(self, recipe_ids: list[str]) -> list[dict]:
-        pass
+    def get_and_save_recipes_from_source(self, source: Source) -> None:
+        if not (
+            recipe_source_retriever := self._recipe_source_retrievers.get(source)
+        ):
+            raise ValueError(f"No recipe source retriever found for source: {source}")
+
+        recipes = recipe_source_retriever.get_recipes()
+        for recipe in recipes:
+            self._recipe_annotation_pipeline(self._get_recipe_text_from_recipe_dict(recipe))
+
+    def _get_recipe_text_from_recipe_dict(self, recipe_dict: dict) -> str:
+        return "\n\n".join([f"{k.upper()}\n{k}: {v}" for k, v in recipe_dict.items()])
+
 
